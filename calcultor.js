c8f5ad39ff29e92f7379c80bd6544b97fe1b50a9
Birendra <40135762+Birendra-Paswan@users.noreply.github.com>
2021-07-01 17:09:21 +0530

    calculator files

---
+++ calcultor.js
@@ -0,0 +1,21 @@
+// jshint esversion:6
+const express=require("express");
+const bodyParser=require("body-parser");
+
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req,res){
+    var num1=Number(req.body.n1);
+    var num2=Number(req.body.n2);
+    var result=num1+num2;
+    res.send("The sum of the numberes is "+ result);
+});
+app.listen(3000,function(){
+    console.log("Server start on 3000 port");
+});
